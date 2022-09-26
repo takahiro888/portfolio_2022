b7c0313604d728a06801f24824d856a55fc6d54c
u <u@us-Macintosh.local>
2022-09-27 02:22:08 +0900

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Memo;
+use App\Models\Tag;
+use App\Models\MemoTag;
+use DB;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+          $tags = Tag::where('user_id','=',\Auth::id())->whereNull('deleted_at')
+          ->orderBy('id','DESC')->get();
+
+        return view('create',compact('tags'));
+    }
+
+    public function store(Request $request)
+    {
+        $posts = $request->all();
+        $request->validate(['content' => 'required']);
+        // dump dieの略 →　メソッドの引数の取った値を展開して止める　→ データ確認
+
+        // =====ここからトランザクション開始====
+        DB::transaction(function() use($posts){
+            // メモIDをインサートして取得
+            $memo_id = Memo::insertGetId(['content' => $posts['content'], 'user_id' => \Auth::id()]);
+            $tag_exists = Tag::where('user_id','=', \Auth::id())->where('name','=', $posts['new_tag'])
+            ->exists();
+            // 新規タグが入力されているかチェック
+            // 新規タグが既にtagsテーブルに存在するのかチェック
+            if(!empty($posts['new_tag']) && !$tag_exists ){
+                // 新規タグが既にtagsテーブルにインサート→IDを取得
+                $tag_id = Tag::insertGetId(['user_id' => \Auth::id(), 'name' => $posts['new_tag']]);
+                // memo_tagsにインサートして、メモとタグを紐付ける
+                MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag_id]);
+            }
+            // 既存タグが紐付けられた場合→memo_tagsにインサート
+            if(!empty($posts['tags'][0])){
+            foreach($posts['tags'] as $tag){
+                MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag]);
+            }
+        }
+        });
+        // ====ここまでがトランザクションの範囲====
+        return redirect( route('home'));
+    }
+
+    public function edit($id)
+    {
+          $edit_memo = Memo::select('memos.*','tags.id AS tag_id')
+          ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+          ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
+          ->where('memos.user_id', '=',\Auth::id())
+          ->where('memos.id', '=',$id)
+          ->whereNull('memos.deleted_at')
+          ->get();
+
+          $include_tags = [];
+          foreach($edit_memo as $memo){
+            array_push($include_tags, $memo['tag_id']);
+          }
+
+          $tags = Tag::where('user_id','=',\Auth::id())->whereNull('deleted_at')->orderBy('id','DESC')
+          ->get();
+        return view('edit',compact('edit_memo','include_tags','tags'));
+    }
+
+
+    public function update(Request $request)
+    {
+        $posts = $request->all();
+        $request->validate(['content' => 'required']);
+        // トランザクションスタート
+        DB::transaction(function() use($posts){
+        Memo::where('id', $posts['memo_id'])->update(['content' => $posts['content']]);
+        // 一旦メモとタグの紐付けを削除
+        MemoTag::where('memo_id', '=',$posts['memo_id'])->delete();
+        // 再度メモとタグの紐付け
+        foreach ($posts['tags'] as $tag){
+            MemoTag::insert(['memo_id' =>$posts['memo_id'],'tag_id' => $tag]);
+        }
+        // もし、新しいタグの入力があれば、インサートして紐付ける
+        $tag_exists = Tag::where('user_id', '=', \Auth::id())->where('name','=',$posts['new_tag'])
+        ->exists();
+        // 新規タグが既にtagsテーブルにインサートIDを取得
+        if(!empty($posts['new_tag']) && !$tag_exists ){
+            // 新規タグが既に存在しなければ、tagsテーブルにインサートIDを取得
+            $tag_id = Tag::insertGetId(['user_id' => \Auth::id(),'name' => $posts['new_tag']]);
+            // memo_tagsにインサートして、メモとタグを紐付ける
+            MemoTag::insert(['memo_id' => $posts['memo_id'], 'tag_id' => $tag_id]);
+        }
+    });
+        // トランザクションここまで
+        return redirect(route('home'));
+    }
+
+
+    public function destory(Request $request)
+    {
+        $posts = $request->all();
+        Memo::where('id',$posts['memo_id']) -> update(['deleted_at' => date("Y-m-d H:i:s", time())]);
+        return redirect( route('home'));
+    }
+
+}
---
+++ resources/views/edit.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('javascript')
+<script src="/js/confirm.js"></script>
+@endsection
+
+@section('content')
+<div class="card">
+  <div class="card-header d-flex justify-content-between">
+    メモ編集
+    <form id="delete-form" action="{{ route('destory') }}" method="POST">
+      @csrf
+      <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
+      <i class="fa-solid fa-trash mr-3"  onclick="deleteHandle(event);"></i>
+    </form>
+  </div>
+  <!-- {{ route ('store')}}と書くと/store -->
+  <form class="card-body my-card-body" action="{{ route('update')}}" method="POST">
+    @csrf
+    <input type="hidden" name="memo_id" class="form-control" value="{{ $edit_memo[0]['id'] }}" />
+    <div class="form-group">
+      <textarea class="form-control w-50 mb-3" name="content" rows="3" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
+    </div>
+    @error('content')
+    <div class="alert alert-danger">メモ内容を入力してください！</div>
+    @enderror
+    @foreach($tags as $t)
+    <div class="form-check form-check-inline mb-3">
+      <!-- ３項演算子　→ if文を１行で書く方法 条件　? trueだったら : falseだったら -->
+      <!-- もし$include_tagsにループで回っているタグのidが含まれれば、checkedを書く -->
+      <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $t['id'] }}" 
+      value="{{ $t['id'] }}" {{ in_array($t['id'], $include_tags, true) ? 'checked' : '' }}>
+      <label class="form-check-label" for="{{ $t['id'] }}">{{ $t['name']}}</label>
+    </div>
+    @endforeach
+    <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力" />
+    <button type="submit" class="btn btn-primary">更新</button>
+  </form>
+</div>
+@endsection
